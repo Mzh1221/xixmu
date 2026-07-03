c2055f16197709a390786a510e693c4407d17046
Mzh1221 <2672974645@qq.com>
2026-07-03 23:13:10 +0800

    vault backup: 2026-07-03 23:13:09

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -11,7 +11,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	showInNavbar: true,
 
 	// 使用方式："meting" 使用 Meting API，"local" 使用本地音乐列表
-	mode: "meting",
+	mode: "local",
 
 	// 默认音量 (0-1)
 	volume: 0.7,
@@ -56,6 +56,13 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 				cover: "/assets/music/cover/109951169585655912.webp",
 				lrc: "",
 			},
+			{
+				name: "luv(sic.) pt.3",
+				artist: "Nujabes / Shing02",
+				url: "/assets/music/luv(sic.)pt.3.mp3",
+				cover: "/assets/music/cover/luv.jpg",
+				lrc: "",
+			}
 		],
 	},
 };

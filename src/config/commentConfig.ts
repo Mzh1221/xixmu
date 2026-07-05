cd693ab86223a70b50d800903addfd6d15156fd3
Mzh1221 <2672974645@qq.com>
2026-07-05 14:33:02 +0800

    vault backup: 2026-07-05 14:33:02

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -23,7 +23,7 @@ export const commentConfig: CommentConfig = {
 	//waline评论系统配置
 	waline: {
 		// waline 后端服务地址
-		serverURL: "https://xixmu-waline.vercel.app/",
+		serverURL: "https://comment.162351.xyz/",
 		// 设置 Waline 评论系统语言
 		lang: "zh-CN",
 		// 设置 Waline 评论系统表情地址

9b7267da0f28ec190b37293c4f8d5e51dd5d62f9
Mzh1221 <2672974645@qq.com>
2026-07-11 13:27:07 +0800

    logo

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -73,7 +73,7 @@ export const siteConfig: SiteConfig = {
 		// 3. 本地图片（src目录，自动优化但会增加构建时间）: { type: "image", value: "assets/images/logo.webp", alt: "Logo" }
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
-			 type: "icon", value: "material-symbols:home-pin-outline"
+			 type: "icon", value: "line-md:check-all"
 			// type: "image",
 			// value: "assets/images/firefly.png",
 			// alt: "🍀",

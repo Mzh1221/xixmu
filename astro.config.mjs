26ea000f6341d286fa081421220bfa5f77e6d404
Mzh1221 <2672974645@qq.com>
2026-07-24 14:54:56 +0800

    fix: reset base to / for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -58,7 +58,7 @@ const adapter = process.env.CF_WORKERS
 export default defineConfig({
 	site: "https://gh.162351.xyz",
 
-	base: "/xixmu/",
+	base: "/",
 	trailingSlash: "always",
 
 	// 字体配置 - 只加载实际使用的字体，跳过未引用的以加快构建

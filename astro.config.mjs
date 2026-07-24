dedb70bfa4fedb9eb682254094babd12ca1bf755
Mzh1221 <2672974645@qq.com>
2026-07-24 15:57:48 +0800

    fix: update site url to apex domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -56,7 +56,7 @@ const adapter = process.env.CF_WORKERS
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://gh.162351.xyz",
+	site: "https://162351.xyz",
 
 	base: "/",
 	trailingSlash: "always",

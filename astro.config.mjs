842da9ff384e85a57f3ae589a57004dc49cca23c
Mzh1221 <2672974645@qq.com>
2026-07-28 17:19:48 +0800

    vault backup: 2026-07-28 17:19:48

--- astro.config.mjs
+++ astro.config.mjs
@@ -64,9 +64,8 @@ const adapter = process.env.CF_WORKERS
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://xixmu.top",
-
-	base: "/",
+	site: "mzh1221.github.io",
+	base: "/xixmu",
 	trailingSlash: "always",
 
 	// 字体配置 - 只加载实际使用的字体，跳过未引用的以加快构建

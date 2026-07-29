dc7c60c71e3b16377e91bc0711a0585bfc37959d
Mzh1221 <2672974645@qq.com>
2026-07-29 08:16:54 +0800

    fix: site url with https

--- astro.config.mjs
+++ astro.config.mjs
@@ -64,8 +64,8 @@ const adapter = process.env.CF_WORKERS
 
 // https://astro.build/config
 export default defineConfig({
-	site: "mzh1221.github.io",
-	base: "/xixmu",
+	site: "https://mzh1221.github.io",
+	base: "/xixmu/",
 	trailingSlash: "always",
 
 	// 字体配置 - 只加载实际使用的字体，跳过未引用的以加快构建

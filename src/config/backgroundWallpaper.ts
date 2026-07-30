5eaf4cd0f81ba6ddc9ef6222c4e1222655934822
Mzh1221 <2672974645@qq.com>
2026-07-30 10:12:05 +0800

    vault backup: 2026-07-30 10:12:05

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -79,7 +79,11 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 主页横幅主标题字体大小
 			titleSize: "4.5rem",
 			// 主页横幅副标题
-			subtitle: ["在记忆干枯前描绘。", "有惊无险，失而复得，虚惊一场。"],
+			subtitle: [
+				"在记忆干枯前描绘。",
+				"有惊无险，失而复得，虚惊一场。",
+				"我MYGO了。"
+			],
 			// subtitle: [
 			// 	"In Reddened Chrysalis, I Once Rest",
 			// 	"From Shattered Sky, I Free Fall",

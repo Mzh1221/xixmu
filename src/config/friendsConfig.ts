31c18ae90ea296b65df2472da351e8894e0e91cc
Mzh1221 <2672974645@qq.com>
2026-08-04 15:55:14 +0800

    加年华友链

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -46,12 +46,22 @@ export const friendsConfig: FriendLink[] = [
 		title: "Xixmu",
 		imgurl:
 			"https://xixmu.top/_astro/head_ima.rsW3s28l_1KtIxl.avif",
-		desc: "在记忆干枯前描绘",
+		desc: "在记忆干枯前描绘。",
 		siteurl: "https://xixmu.top",
 		tags: ["Blog"],
 		weight: 1, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
 	},
+	{
+		title: "年华",
+		imgurl:
+			"https://q1.qlogo.cn/g?b=qq&nk=1323860289&s=640",
+		desc: "分享生活和技术。",
+		siteurl: "https://blog.amamo.top",
+		tags: ["Blog"],
+		weight: 1, // 权重，数字越大排序越靠前
+		enabled: true, // 是否启用
+	}
 ];
 
 // 获取启用的友链并进行排序

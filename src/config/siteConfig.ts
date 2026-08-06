4be4f5e7312f6ddd51db5ee55d9e7cc1d7c1eaa3
Mzh1221 <2672974645@qq.com>
2026-08-07 00:09:02 +0800

    favicon

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -55,7 +55,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/firefly-32.png",
+			src: "https://s41.ax1x.com/2026/07/11/pmsDpgU.png",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
